4047d0f615881840957ac300d7173f76e132bcfd
Suren Baghdasaryan <surenb@google.com>
2018-01-09 15:47:40 -0800

    aura: Set lmkd configuration properties
    Bug: 63631020
    
    Change-Id: I16a183e8e4625e8b98cefdc9c8a7b650556a2b55
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- props.mk
+++ props.mk
@@ -179,6 +179,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.text_small_cache_height=1024 \
     ro.hwui.text_small_cache_width=1024
 
+# LMKD
+PRODUCT_PRODUCT_PROPERTY_OVERRIDES += \
+   ro.lmk.low=1001 \
+   ro.lmk.medium=0 \
+   ro.lmk.critical=0 \
+   ro.lmk.critical_upgrade=false \
+   ro.lmk.upgrade_pressure=100 \
+   ro.lmk.downgrade_pressure=100 \
+   ro.lmk.kill_heaviest_task=true \
+   ro.lmk.kill_timeout_ms=100
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     audio.offload.video=true \
