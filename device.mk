6f635eb9412667d327fbfffe781f3f3464322878
Demon Singur <demonsingur@gmail.com>
2019-04-17 16:29:09 +0800

    aura: Import fingerprint HIDL
    Change-Id: I733eea70dc637fa61c2e3a8036100de7febd71a9

--- device.mk
+++ device.mk
@@ -237,7 +237,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service.aura
 
 # Health
 PRODUCT_PACKAGES += \
