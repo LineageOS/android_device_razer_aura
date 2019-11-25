b96cc2977e5a272aa555d304f2dcc28728f6039a
Bruno Martins <bgcngm@gmail.com>
2019-11-25 22:34:31 +0000

    aura: Set up WiFi and build the HAL from sources
    Change-Id: I7b92eaea2cf2f059c8214916ea186dc6c1e03917

--- device.mk
+++ device.mk
@@ -339,7 +339,12 @@ PRODUCT_PACKAGES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service
+    android.hardware.wifi@1.0-service \
+    hostapd \
+    libwifi-hal-qcom \
+    libwpa_client \
+    wpa_supplicant \
+    wpa_supplicant.conf
 
 # WiFi Display
 PRODUCT_PACKAGES += \
