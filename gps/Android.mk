dfb80a1190e943528f6e1354f963152afbc1c648
Saurabh Srivastava <ssrivast@codeaurora.org>
2019-12-18 14:26:29 +0530

    aura: gps: Android mk to bp conversion
    Change-Id: I206b4a5a2b7e88a6a20522611e7b2684594ef6f4
    CRs-Fixed: 2559087

--- gps/Android.mk
+++ gps/Android.mk
@@ -1,6 +1,11 @@
 ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-LOCAL_PATH := $(call my-dir)
-include $(LOCAL_PATH)/build/target_specific_features.mk
 
+# Set required flags
+GNSS_CFLAGS := \
+    -Werror \
+    -Wno-undefined-bool-conversion
+
+LOCAL_PATH := $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
+
+endif # ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
