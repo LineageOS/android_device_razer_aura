678e3f56786e148a3d801db34502d7c965498991
Michael Bestas <mkbestas@lineageos.org>
2020-07-13 20:01:32 +0300

    aura: Remove livedisplay HAL
    Change-Id: Iad92cc65c6e1f4c5e76f4eaf51d6c4a2388d819d

--- device.mk
+++ device.mk
@@ -228,7 +228,6 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.xiaomi_sdm845 \
     vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
