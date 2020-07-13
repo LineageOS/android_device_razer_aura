e56c20d364452cdfbfd8b52b62440e4df355de8c
Michael Bestas <mkbestas@lineageos.org>
2020-07-13 20:15:48 +0300

    aura: Remove custom power HAL
    Change-Id: I52d3b78cc97927ad66c1c11e769b1f80a4f4e577

--- device.mk
+++ device.mk
@@ -283,10 +283,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.xiaomi_sdm845
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    android.hardware.power@1.1-service-qti
 
 # QTI
 PRODUCT_COPY_FILES += \
