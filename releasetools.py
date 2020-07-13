18f80b7be6fa60105a10e5c927a024fddfb2e07c
Michael Bestas <mkbestas@lineageos.org>
2020-07-13 20:26:37 +0300

    aura: Remove modem assertions
    Change-Id: Ia60e57375ddec0eaa2ca6fed94a7eda679cf6e89

--- releasetools.py
+++ releasetools.py
@@ -24,14 +24,6 @@ def IncrementalOTA_InstallEnd(info):
   OTA_InstallEnd(info)
   return
 
-def FullOTA_Assertions(info):
-  AddModemAssertion(info, info.input_zip)
-  return
-
-def IncrementalOTA_Assertions(info):
-  AddModemAssertion(info, info.target_zip)
-  return
-
 def AddImage(info, basename, dest):
   path = "IMAGES/" + basename
   if path not in info.input_zip.namelist():
@@ -44,17 +36,4 @@ def AddImage(info, basename, dest):
 
 def OTA_InstallEnd(info):
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
-  return
-
-def AddModemAssertion(info, input_zip):
-  android_info = info.input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info)
-  miui_version = re.search(r'require\s+version-miui\s*=\s*(.+)', android_info)
-  if m and miui_version:
-    timestamp = m.group(1).rstrip()
-    firmware_version = miui_version.group(1).rstrip()
-    if ((len(timestamp) and '*' not in timestamp) and \
-        (len(firmware_version) and '*' not in firmware_version)):
-      cmd = 'assert(xiaomi.verify_modem("{}") == "1" || abort("ERROR: This package requires firmware from MIUI {} developer build or newer. Please upgrade firmware and retry!"););'
-      info.script.AppendExtra(cmd.format(timestamp, firmware_version))
   return
