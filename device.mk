a93d0c82552c62a8da279443066d3db03cbbded0
LuK1337 <priv.luk@gmail.com>
2020-09-18 14:36:05 +0200

    aura: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- device.mk
+++ device.mk
@@ -247,7 +247,11 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0_system
+    android.hidl.base@1.0_system \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # Input
 PRODUCT_COPY_FILES += \
