4874a55c3867ba9b6588a137b52a1afc53946a8d
LuK1337 <priv.luk@gmail.com>
2020-09-18 13:12:09 +0200

    aura: Drop android.hardware.thermal HAL
    android.hardware.thermal@1.0::IThermal/default is deprecated in compatibility matrix at FCM Version 4; it should not be served.
    
    Change-Id: Icc3c672a0bf6b9e4ee76ccf91e6cdca5fb778fde

--- device.mk
+++ device.mk
@@ -375,12 +375,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sdm845
-
 # Treble
 PRODUCT_USE_VNDK_OVERRIDE := true
 
