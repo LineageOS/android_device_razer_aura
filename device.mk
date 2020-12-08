90f19df61decc530a020624c2fefd36f9ff3b137
Bruno Martins <bgcngm@gmail.com>
2020-12-08 22:03:48 +0000

    aura: Migrate to Health 2.1 HAL
    Change-Id: Ic6302844ac58c16713b43b6871a705b9b4749a96

--- device.mk
+++ device.mk
@@ -241,8 +241,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl:64 \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl:64 \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \
