fef1232e00fe467965a362b8d86f3694f639551b
Michael Bestas <mkbestas@lineageos.org>
2020-12-19 02:53:35 +0200

    aura: Switch to standalone extract utils
    Align with templates while we are at it
    
    Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4

--- extract-files.sh
+++ extract-files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2018-2020 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -14,37 +15,20 @@ VENDOR=razer
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}"/../../..
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
 source "${HELPER}"
 
-function blob_fixup() {
-    case "${1}" in
-    product/etc/permissions/telephonyservice.xml)
-        sed -i 's|/system/framework/QtiTelephonyServicelibrary.jar|/product/framework/QtiTelephonyServicelibrary.jar|g' "${2}"
-        ;;
-    vendor/etc/gpfspath_oem_config.xml)
-        sed -i 's|/vendor/securefs/data/|/mnt/vendor/securefs/data/|g' "${2}"
-        ;;
-    vendor/lib/libgps.utils.so)
-        patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
-        ;;
-    vendor/lib64/libgps.utils.so)
-        patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
-        ;;
-    esac
-}
-
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
-SECTION=
 KANG=
+SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
@@ -69,10 +53,26 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
-# Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
+function blob_fixup() {
+    case "${1}" in
+        product/etc/permissions/telephonyservice.xml)
+            sed -i 's|/system/framework/QtiTelephonyServicelibrary.jar|/product/framework/QtiTelephonyServicelibrary.jar|g' "${2}"
+            ;;
+        vendor/etc/gpfspath_oem_config.xml)
+            sed -i 's|/vendor/securefs/data/|/mnt/vendor/securefs/data/|g' "${2}"
+            ;;
+        vendor/lib/libgps.utils.so)
+            patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
+            ;;
+        vendor/lib64/libgps.utils.so)
+            patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
+            ;;
+    esac
+}
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
-        "${KANG}" --section "${SECTION}"
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
