ebbe769080df58a70cad4cef797a3ab90452ab88
dianlujitao <dianlujitao@lineageos.org>
2021-02-09 18:14:04 +0800

    aura: Switch to source built vendor.qti.hardware.perf@1.0
    For some reason, any prebuilt ones from pre-R ROMs no longer work on R
    and cause the following error:
      E ANDR-PERF: Unable to link to gPerfHal death notifications!
    
    Change-Id: Ia5de8c300d71b6def223e97ef9d58898c9c11a57

--- device.mk
+++ device.mk
@@ -321,7 +321,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    vendor.qti.hardware.perf@1.0.vendor
 
 # Protobuf
 PRODUCT_PACKAGES += \
