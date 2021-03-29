4db9fcfccd62d5d8793da69dda389ea06e6c04a7
Michael Bestas <mkbestas@lineageos.org>
2021-03-29 21:44:12 +0300

    Revert "aura: Split RF configs from the main NFC config"
    This reverts commit 717167f23e8721c47111cfa6e4186633e4b781ce.
    
    Change-Id: Iaaf3582c25421559ef5424ef70d44ebf7952f0dd

--- device.mk
+++ device.mk
@@ -317,7 +317,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
-    $(LOCAL_PATH)/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/libnfc-nxp_RF.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # Power
