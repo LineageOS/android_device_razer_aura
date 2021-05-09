7917242e97b2b1d4c4626dcfb9e202d4fdca1b61
Nolen Johnson <johnsonnolen@gmail.com>
2021-05-09 16:04:34 -0400

    aura: Simplify AVB flag logic
    * AVB's `--flags 3` argument builds a disabled VBMeta with
      disabled hashtree, so no need to set both flags individually.
    
    Change-Id: Ibe9ee322a5af58f13e76d53e18dee49451fe8838

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,8 +10,7 @@ DEVICE_PATH := device/razer/aura
 
 # Android Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
 # Architecture
 TARGET_ARCH := arm64
