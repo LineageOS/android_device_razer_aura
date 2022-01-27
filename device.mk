620a212474cde1e05806558c940aeb7733915d4c
Alexander Koskovich <akoskovich@pm.me>
2022-01-27 14:23:56 -0700

    aura: Drop support for Snapdragon Camera.
     * Won't be implemented this cycle (or ever again hopefully).
    
    Change-Id: Ie2a346319dfe814deaf9833cb31640505cc6b7dc

--- device.mk
+++ device.mk
@@ -177,7 +177,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     libgui_shim \
-    Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Control groups and task profiles
