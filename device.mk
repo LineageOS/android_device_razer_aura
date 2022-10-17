dc8889ab44b3a2693f613c8248808af9707a5883
Alexander Martinz <amartinz@shiftphones.com>
2022-10-17 18:16:15 +0200

    Revert "aura: Disable APEX compression"
    This reverts commit e77a027df96b26a633908138b9c4331f56bc082e.
    
    Compressed APEX support is fixed kernel side.
    
    Test: build, flash and boot device with flag reverted.
    Test: boot GSI with compressed APEX enabled.
    
    Change-Id: I5988614cc055d565b17a5c8d830e35feb0e33e92
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- device.mk
+++ device.mk
@@ -12,10 +12,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Disable APEX compression
-# Keep this after including updatable_apex.mk
-PRODUCT_COMPRESSED_APEX := false
-
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/razer/aura/aura-vendor.mk)
 
