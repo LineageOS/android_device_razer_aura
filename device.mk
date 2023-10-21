f3f0bfefc3b2adf6e9e14c4a3ade84c7bd919e2e
LuK1337 <priv.luk@gmail.com>
2023-10-21 15:44:06 +0200

    aura: Build android.hardware.bluetooth@1.0
    Required by libantradio.
    
    Change-Id: I5e26a4154cf54e64f424cb6cf20db87b7023f1cd

--- device.mk
+++ device.mk
@@ -139,6 +139,7 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0 \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.a2dp@1.0.vendor \
     android.hardware.bluetooth.audio@2.0-impl:32 \
