b41b945d3e025387ef6d88716f9199bf0d82ef7c
LuK1337 <priv.luk@gmail.com>
2023-11-01 18:11:13 +0100

    aura: Use libutils-v33 for pm-service
    Change-Id: Ia998fa72d8747de18010abe6491d2c374cb7aa0a

--- extract-files.sh
+++ extract-files.sh
@@ -82,6 +82,9 @@ function blob_fixup() {
         system_ext/lib64/libdpmframework.so)
             sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
             ;;
+        vendor/bin/pm-service)
+            grep -q libutils-v33.so "${2}" || "${PATCHELF}" --add-needed "libutils-v33.so" "${2}"
+            ;;
         vendor/lib/hw/audio.primary.sdm845.so)
             "${PATCHELF}" --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
             "${PATCHELF}" --replace-needed "libtinycompress_vendor.so" "libtinycompress.so" "${2}"
