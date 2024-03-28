569e9088940d1581c7ad865043a29a0c52b2399d
Bruno Martins <bgcngm@gmail.com>
2024-03-28 14:12:52 +0000

    aura: Mark CNE symlink target as required package
    Change-Id: I0bfe65ba61b6aa9916fe6459be2a84d772939a2f

--- device.mk
+++ device.mk
@@ -288,12 +288,14 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
 # QTI
-PRODUCT_PACKAGES += \
-    libqti_vndfwk_detect.vendor
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/permissions/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
+
+# QTI fwk-detect
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti.vendor # Needed by CNE app
 
 # Radio
 PRODUCT_PACKAGES += \
