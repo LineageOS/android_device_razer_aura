586c6fdefb82682f3963792f8a19128a8ac9e112
Bruno Martins <bgcngm@gmail.com>
2024-06-23 21:58:49 +0100

    aura: Convert WiFi firmware symlinks to install_symlink targets
    The last bit that was preventing building hermetic images.
    
    Change-Id: I75bb7de4593a7992f6015317cf97e088beeba0cb

--- device.mk
+++ device.mk
@@ -469,3 +469,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+# WiFi firmware symlinks
+PRODUCT_PACKAGES += \
+    firmware_wlan_mac.bin_symlink \
+    firmware_WCNSS_qcom_cfg.ini_symlink
