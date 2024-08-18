892da21d81b9e6cc8ad625ada59d99363b8b61cd
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-18 09:52:31 +0300

    aura: skip dependencies which aren't defined in .bp
    Change-Id: I01a653661856495788c05b720c0577dac44998a4

--- extract-files.py
+++ extract-files.py
@@ -44,6 +44,11 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.hardware.wifidisplaysession@1.0',
     ): lib_fixup_vendor_suffix,
     (
+        'libOmxCore',
+        'libc2dcolorconvert',
+        'libplatformconfig',
+        'libril',
+        'libwpa_client',
     ): lib_fixup_remove,
 }
 
