cfeb6e3863e96d965516dc2af4219afb6b40a870
Michael Bestas <mkbestas@lineageos.org>
2024-10-09 14:23:11 +0300

    aura: Align extract scripts again
    Change-Id: Ifc0d464e1efbfcc26ee69cb9708f7c22c8c7eefc

--- extract-files.sh
+++ extract-files.sh
@@ -2,7 +2,6 @@
 #
 # SPDX-FileCopyrightText: 2016 The CyanogenMod Project
 # SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
-#
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -16,6 +15,10 @@ MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
+
+# If XML files don't have comments before the XML header, use this flag
+# Can still be used with broken XML files by using blob_fixup
+export TARGET_DISABLE_XML_FIXING=true
 
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
@@ -33,23 +36,23 @@ SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
-        --only-firmware )
-                ONLY_FIRMWARE=true
-                ;;
-        -n | --no-cleanup )
-                CLEAN_VENDOR=false
-                ;;
-        -k | --kang )
-                KANG="--kang"
-                ;;
-        -s | --section )
-                SECTION="${2}"
-                shift
-                CLEAN_VENDOR=false
-                ;;
-        * )
-                SRC="${1}"
-                ;;
+        --only-firmware)
+            ONLY_FIRMWARE=true
+            ;;
+        -n | --no-cleanup)
+            CLEAN_VENDOR=false
+            ;;
+        -k | --kang)
+            KANG="--kang"
+            ;;
+        -s | --section)
+            SECTION="${2}"
+            shift
+            CLEAN_VENDOR=false
+            ;;
+        *)
+            SRC="${1}"
+            ;;
     esac
     shift
 done
@@ -69,7 +72,13 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
             ;;
-        vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti|vendor/bin/hw/vendor.qti.hardware.sensorscalibrate@1.0-service|vendor/bin/hw/vendor.qti.hardware.iop@2.0-service|vendor/bin/hw/vendor.qti.hardware.factory@1.0-service|vendor/bin/hw/vendor.qti.hardware.tui_comm@1.0-service-qti|vendor/bin/hw/vendor.qti.hardware.qteeconnector@1.0-service|vendor/bin/hw/vendor.qti.esepowermanager@1.0-service)
+        vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti | \
+            vendor/bin/hw/vendor.qti.hardware.sensorscalibrate@1.0-service | \
+            vendor/bin/hw/vendor.qti.hardware.iop@2.0-service | \
+            vendor/bin/hw/vendor.qti.hardware.factory@1.0-service | \
+            vendor/bin/hw/vendor.qti.hardware.tui_comm@1.0-service-qti | \
+            vendor/bin/hw/vendor.qti.hardware.qteeconnector@1.0-service | \
+            vendor/bin/hw/vendor.qti.esepowermanager@1.0-service)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
             ;;
@@ -98,7 +107,15 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
             ;;
-        vendor/lib*/vendor.qti.hardware.iop@1.0.so|vendor/lib*/libqti-iopd.so|vendor/lib*/libqti-iopd-client.so|vendor/lib*/hw/vendor.qti.hardware.factory@1.0-impl.so|vendor/lib*/libsecureui_svcsock.so|vendor/lib*/libQTEEConnector_vendor.so|vendor/lib*/libGPQTEEC_vendor.so|vendor/lib*/hw/vendor.qti.hardware.qteeconnector@1.0-impl.so|vendor/lib*/hw/vendor.qti.esepowermanager@1.0-impl.so)
+        vendor/lib*/vendor.qti.hardware.iop@1.0.so | \
+            vendor/lib*/libqti-iopd.so | \
+            vendor/lib*/libqti-iopd-client.so | \
+            vendor/lib*/hw/vendor.qti.hardware.factory@1.0-impl.so | \
+            vendor/lib*/libsecureui_svcsock.so | \
+            vendor/lib*/libQTEEConnector_vendor.so | \
+            vendor/lib*/libGPQTEEC_vendor.so | \
+            vendor/lib*/hw/vendor.qti.hardware.qteeconnector@1.0-impl.so | \
+            vendor/lib*/hw/vendor.qti.esepowermanager@1.0-impl.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
             ;;
@@ -109,6 +126,7 @@ function blob_fixup() {
 
     return 0
 }
+
 function blob_fixup_dry() {
     blob_fixup "$1" ""
 }
