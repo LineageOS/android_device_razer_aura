3a622ba8d44a8860b87e9095758a61f4d695cb3a
LuK1337 <priv.luk@gmail.com>
2024-10-15 22:04:03 +0200

    aura: Switch to py extract utils
    * Disable elfchecks for now.
    * Use patchelf 0.9 and even 0.18 has issues with
      several of our libs.
    
    Change-Id: Ia13b728464495ff208ca1a706847b11af5c93ae8

---
+++ extract-files.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import extract_utils.tools
+extract_utils.tools.DEFAULT_PATCHELF_VERSION = '0_9'
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    ('system_ext/lib/com.qualcomm.qti.ant@1.0.so', 'system_ext/lib64/com.qualcomm.qti.ant@1.0.so', 'vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti', 'vendor/bin/hw/vendor.qti.esepowermanager@1.0-service', 'vendor/bin/hw/vendor.qti.hardware.factory@1.0-service', 'vendor/bin/hw/vendor.qti.hardware.iop@2.0-service', 'vendor/bin/hw/vendor.qti.hardware.qteeconnector@1.0-service', 'vendor/bin/hw/vendor.qti.hardware.sensorscalibrate@1.0-service', 'vendor/bin/hw/vendor.qti.hardware.tui_comm@1.0-service-qti', 'vendor/lib/libGPQTEEC_vendor.so', 'vendor/lib64/libGPQTEEC_vendor.so', 'vendor/lib/libQTEEConnector_vendor.so', 'vendor/lib64/libQTEEConnector_vendor.so', 'vendor/lib/libqti-iopd.so', 'vendor/lib64/libqti-iopd.so', 'vendor/lib/libqti-iopd-client.so', 'vendor/lib64/libqti-iopd-client.so', 'vendor/lib/libsecureui_svcsock.so', 'vendor/lib64/libsecureui_svcsock.so', 'vendor/lib/vendor.qti.hardware.iop@1.0.so', 'vendor/lib64/vendor.qti.hardware.iop@1.0.so', 'vendor/lib/hw/vendor.qti.esepowermanager@1.0-impl.so', 'vendor/lib64/hw/vendor.qti.esepowermanager@1.0-impl.so', 'vendor/lib/hw/vendor.qti.hardware.factory@1.0-impl.so', 'vendor/lib64/hw/vendor.qti.hardware.factory@1.0-impl.so', 'vendor/lib/hw/vendor.qti.hardware.qteeconnector@1.0-impl.so', 'vendor/lib64/hw/vendor.qti.hardware.qteeconnector@1.0-impl.so'): blob_fixup()
+        .replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
+    'system_ext/lib/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V3-cpp.so'),
+    ('system_ext/lib64/lib-imscamera.so', 'system_ext/lib64/lib-imsvideocodec.so'): blob_fixup()
+        .add_needed('libgui_shim.so')
+        .replace_needed('libqdMetaData.so', 'libqdMetaData.system.so'),
+    'vendor/lib/hw/audio.primary.sdm845.so': blob_fixup()
+        .replace_needed('libcutils.so', 'libprocessgroup.so')
+        .replace_needed('libtinycompress_vendor.so', 'libtinycompress.so'),
+    ('vendor/lib/libbthost_if.so', 'vendor/lib64/libbthost_if.so'): blob_fixup()
+        .fix_soname(),
+    ('vendor/lib/libgps.utils.so', 'vendor/lib64/libgps.utils.so'): blob_fixup()
+        .replace_needed('libcutils.so', 'libprocessgroup.so'),
+    ('vendor/lib64/libwvhidl.so', 'vendor/lib/mediadrm/libwvdrmengine.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'aura',
+    'razer',
+    blob_fixups=blob_fixups,
+    add_firmware_proprietary_file=True,
+    check_elf=False,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
