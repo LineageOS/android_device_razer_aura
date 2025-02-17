9d89e27605f2502fb98e1632c7f5f619783362ca
LuK1337 <priv.luk@gmail.com>
2025-02-17 23:07:57 +0100

    aura: Migrate to avbtool --set_verification_disabled_flag
    Change-Id: Ib73f4d97a923b25e25848b24a3554cdd4de1a5ae

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,7 @@ AB_OTA_PARTITIONS += \
 
 # Android Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 
 
 # Architecture
 TARGET_ARCH := arm64
