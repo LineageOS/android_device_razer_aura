f3edcea3f6b779db51057683fdeb22dfe9915006
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-19 02:44:05 +0200

    aura: switch to common QCOM AIDL bootctrl HAL
    Change-Id: I04ab771d3b1c38b58913607fbff1bb3b55e1fe25

--- device.mk
+++ device.mk
@@ -136,13 +136,8 @@ PRODUCT_PACKAGES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl.recovery \
-    bootctrl.sdm845.recovery
-
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl:64 \
-    android.hardware.boot@1.0-service \
-    bootctrl.sdm845
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
@@ -317,7 +312,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/qcom-caf/bootctrl
 
 # Telephony
 PRODUCT_PACKAGES += \
