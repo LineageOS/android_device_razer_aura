42988eb5f7bf54cdd667f263d09bddad244b32ae
basamaryan <basam.aryan@gmail.com>
2025-07-08 15:51:28 -0400

    aura: Override kernel BPF version
    Change-Id: I3c581f3120b6258b3b45020c5ba12f6bf5b0ee27

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -123,6 +123,7 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/devices/platform/soc/a98000.i2c/i2c-7/7-0020/in
 BOARD_USES_QCOM_HARDWARE := true
 
 # Properties
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
