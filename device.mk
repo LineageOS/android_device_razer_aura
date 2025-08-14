edec53f36b3d45890a3c3753d2d65b08d16aa19f
LuK1337 <priv.luk@gmail.com>
2025-08-14 23:47:18 +0200

    aura: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c

--- device.mk
+++ device.mk
@@ -239,7 +239,7 @@ PRODUCT_COPY_FILES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay-service.sdm
 
 # Media
 PRODUCT_PACKAGES += \
