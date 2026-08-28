PixelExperience-Devices/device_razer_aura

aura: RazerParts: Introduce RazerParts app
* will implement Chroma, refresh rate control

RazerParts/src/com/razer/parts/ChromaActivity.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (c) 2021, Alcatraz323 <alcatraz32323@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
+package com.razer.parts;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class ChromaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Fragment fragment = getFragmentManager().findFragmentById(android.R.id.content);
+        ChromaFragment chromaFragment;
+        if (fragment == null) {
+            chromaFragment = new ChromaFragment();
+            getFragmentManager().beginTransaction().add(android.R.id.content, chromaFragment).commit();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

RazerParts/src/com/razer/parts/Constants.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright (c) 2021, Alcatraz323 <alcatraz32323@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
+package com.razer.parts;
+
+public class Constants {
+
+    public static final String SCREEN_RESOLUTION = "screen_resolution";
+    public static final String SCREEN_REFRESH_RATE = "screen_refresh_rate";
+
+    public static final String CHROMA = "chroma";
+
+    public static final String DOLBY_ATMOS = "dolby_atmos";
+    public static final String ACTIVE_WAKE = "active_wakeup";
+
+    public static final String CHROMA_SWITCH = "chroma_switch";
+
+    public static final String CHROMA_MODE = "chroma_mode";
+    public static final String CHROMA_COLOR = "chroma_color";
+    public static final String CHROMA_BRIGHTNESS = "chroma_brightness";
+}

LineageOS/android_device_razer_aura

Change-Id: I468b294fb56acb5749c9eac4c56294a5e626bc01

RazerParts/chroma-controller/src/com/razer/chromacc/ChromaActivity.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (c) 2021, Alcatraz323 <alcatraz32323@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
+package com.razer.chromacc;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class ChromaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Fragment fragment = getFragmentManager().findFragmentById(android.R.id.content);
+        ChromaFragment chromaFragment;
+        if (fragment == null) {
+            chromaFragment = new ChromaFragment();
+            getFragmentManager().beginTransaction().add(android.R.id.content, chromaFragment).commit();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

RazerParts/src/com/razer/parts/Constants.java
@@ -1,0 +1,33 @@
+/*
+ * Copyright (c) 2021, Alcatraz323 <alcatraz32323@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+
+package com.razer.parts;
+
+public class Constants {
+
+    public static final String SCREEN_RESOLUTION = "screen_resolution";
+    public static final String SCREEN_REFRESH_RATE = "screen_refresh_rate";
+
+    public static final String CHROMA = "chroma";
+
+    public static final String DOLBY_ATMOS = "dolby_atmos";
+    public static final String ACTIVE_WAKE = "active_wakeup";
+
+    public static final String BMS_STEP_CHG_SWITCH = "bms_step_charging_switch";
+    public static final String BMS_ALWAYS_CONNECTED_MODE = "bms_always_connected_mode";
+    public static final String BMS_LIMIT_TO_EIGHTY = "bms_limit_to_eighty";
+    public static final String BMS_DISABLE_CHARGING_RIPPLE_EFFECT_SWITCH = "bms_disable_charging_ripple_effect";
+
+    public static final String BMS_OVERLAY_DISABLE_CHARGING_RIPPLE = "com.razer.parts.disablechargingripple";
+}